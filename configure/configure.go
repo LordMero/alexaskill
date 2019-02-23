bb1665bc6a5e28ce2d318481749d4a277b407a33
Git <anotherjonhsmith@gmail.com>
2019-02-23 10:29:30 +0000

    fixed new version of mongodb

--- configure/configure.go
+++ configure/configure.go
@@ -1,9 +1,12 @@
 package configuration
 
 import (
-	"context"
 	"github.com/mongodb/mongo-go-driver/mongo"
+	"github.com/mongodb/mongo-go-driver/mongo/options"
 	"log"
+	"fmt"
+	"context"
+	"time"
 )
 
 var Db *mongo.Database
@@ -11,13 +14,15 @@ var Db *mongo.Database
 const (
 	CONSTRING =  "mongodb://localhost:27017"
 	DBNAME = "babyapp"
-	)
+)
 
 func init()  {
 	// we use the init function to init mongo db connection
-	c, e := mongo.Connect(context.Background(), CONSTRING)
-	if e != nil {log.Fatal(e)}
+	//c, e := mongo.Connect(context.Background(), CONSTRING)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(CONSTRING))
 
-	Db = c.Database(DBNAME)
-
+	if err != nil {log.Panic(err)}
+	Db = client.Database(DBNAME)
+	fmt.Println("connected to mongodb")
 }
